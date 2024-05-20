f1e3fdecc98d491426866ab84beca2b94d848ee5
nivoix <jnivoix@yahoo.fr>
2024-05-20 15:46:10 +0200

    change image profil

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,8 +7,8 @@ const Home = () => {
       <div className="homeContent">
         <div className="content">
           <h1>Jérôme Nivoix</h1>
-          <h2>Développeur web</h2>
-          <h2>/ Intégrateur Web</h2>
+          <h2>Développeur web /</h2>
+          <h2>Intégrateur Web</h2>
           <div className="pdf">
             <a href="./CV.pdf" target="_blank" download>
               Télécharger CV
